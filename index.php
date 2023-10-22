b728fe10426a968f0f6f6d49d52d828f0000b972
Iván <jivanrod@gmail.com>
2023-10-22 15:49:01 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Expresiones simples</title>
+        <meta name="viewport" content="width=device-width">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+    <body>
+        <h2>Funciones de control</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Valores</th>
+                    <th>empty</th>
+                    <th>isset</th>
+                    <th>is_null</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php $x = ""; ?>
+                    <td>$x = ""</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = null; ?>
+                    <td>$x = null o indefinido</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = []; ?>
+                    <td>$x = []</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = [1, 2]; ?>
+                    <td>$x = [1,2]</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = false; ?>
+                    <td>$x = false</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = true; ?>
+                    <td>$x = true</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = 0; ?>
+                    <td>$x = 0</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = "1"; ?>
+                    <td>$x = "1"</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = "0"; ?>
+                    <td>$x = "0"</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+                <tr>
+                    <?php $x = "false"; ?>
+                    <td>$x = "false"</td>
+                    <td class="conversion"><?php var_dump(empty($x)); ?></td>
+                    <td class="conversion"><?php var_dump(isset($x)); ?></td>
+                    <td class="conversion"><?php var_dump(is_null($x)); ?></td>
+                </tr>
+
+            </tbody>
+        </table>
+    </body>
+</html>
